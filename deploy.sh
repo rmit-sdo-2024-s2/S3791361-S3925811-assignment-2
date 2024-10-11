451a061985574ef29fe641e712fb4e03acd448f0
mainPcLinux <s3791361@student.rmit.edu.au>
2024-10-11 15:25:38 +1100

    creates ssh key if needed

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-#
-# Deploy Foo app - see README.md
-#
 
-!/bin/bash
 echo "running the script"
 cd infrastructure/
 terraform init
 terraform validate
 terraform apply
+
+echo "checking for ssh key otherwise creating one"
+
+path_to_ssh_key=~/.ssh/foo_ec2_key
+if [ ! -e "${path_to_ssh_key}" ]
+then
+	echo "Creating SSH key ${path_to_ssh_key}..."
+	ssh-keygen -f "${path_to_ssh_key}" -N ''
+else
+    echo "SSH key ${path_to_ssh_key} already exists."
+
+fi
