2bd062ab87d30465efa04f7c607bd334a7a70cff
mainPcLinux <s3791361@student.rmit.edu.au>
2024-10-11 15:28:44 +1100

    added some comments for the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 echo "running the script"
+
+#running terraform
+
 cd infrastructure/
 terraform init
 terraform validate
 terraform apply
 
+#creates an ssh key if their isnt one present
+
 echo "checking for ssh key otherwise creating one"
 
+#defines the path for the ssh key
 path_to_ssh_key=~/.ssh/foo_ec2_key
+
 if [ ! -e "${path_to_ssh_key}" ]
 then
 	echo "Creating SSH key ${path_to_ssh_key}..."
